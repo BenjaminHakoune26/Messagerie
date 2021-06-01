2b293033cf15bd8169b46d79cf01b27b8b3f2c6f
BenjaminHakoune26 <73535738+BenjaminHakoune26@users.noreply.github.com>
2021-06-01 20:01:06 +0200

    Création de l'espace membre, update des root du projet.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Q
+!src/Repository/UserRepository.php,9\4\943a623b5e2fd95ea78f6287fc600a6f8e0a5235
+P
+ config/packages/translation.yaml,c\7\c7b3dfc088a7c85e5aee83fc03d12b4572887bcb
---
+++ src/Form/ResgisterType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class ResgisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname',TextType::class,[
+                'constraints'=> new Length(2,2,30),
+                'label'=>'Prénom',
+                'attr'=>[
+                    'placeholder'=> 'Votre prénom . . .'
+                ]
+            ])
+            ->add('lastname', TextType::class,[
+                'constraints'=> new Length(2,2,30),
+                'label'=>'Nom',
+                'attr'=>[
+                    'placeholder'=> 'Votre Nom . . .'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'constraints'=> new Length(2,2,50),
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder'=> 'Votre adressse email . . .'
+                ]
+            ])
+            ->add('password', RepeatedType::class,[
+                'type'=>PasswordType::class,
+                'invalid_message'=>'Le mot de passe doit etre identique.',
+                'required'=>true,
+                'first_options'=>[
+                    'label'=>'Mot de passe',
+                    'attr'=>[
+                        'placeholder'=>'Entrer votre mot de passe . . .']],
+                'second_options'=>[
+                    'label'=>'Confirmation du mot de passe',
+                    'attr'=>[
+                        'placeholder'=>'Confirmez votre mot de passe . . .']],
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => "s'inscire"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
